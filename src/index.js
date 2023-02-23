e6e8723b64ede7683502b4857085d1a3f7690fd1
rxq <1909669885@qq.com>
2023-02-23 11:20:34 +0800

    feat: 增加vscode编辑器eslint插件，编写代码时校验js文件，ts文件解析器配置未添加

---
+++ src/index.js
@@ -0,0 +1,2 @@
+// 暂时只有js文件能解析，被vscode的eslint插件识别错误
+// console.log(2)
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-console.log('hello')
+// console.log('hello')
 
 // const a = 1
 //  const b = 2
 export const add = (a: number, b: number) => a + b
+function test() {
+    // console.log(2)
+}
+// console.log('world')
+const arr = []
+
