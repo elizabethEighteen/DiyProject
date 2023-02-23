2b3a6172f5c117c2cb7a2e9d3a089cb9d6c6cd2c
rxq <1909669885@qq.com>
2023-02-23 14:06:40 +0800

    feat: 配置delcare.ts,防止图片import过不了lint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
+import src from '../src/assets/image/1.png'
 // console.log('hello')
 
 // const a = 1
 //  const b = 2
+
+
 export const add = (a: number, b: number) => a + b
 function test() {
     // console.log(2)
 }
 // console.log('world')
 const arr = []
+
+
 
